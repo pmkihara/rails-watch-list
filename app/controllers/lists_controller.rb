8cae088b244e8d0e309afb1068456b01e887e85b
Patricia Kihara <patricia.kihara@outlook.com>
2021-12-18 20:15:18 -0300

    Lists index with CSS

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,16 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
