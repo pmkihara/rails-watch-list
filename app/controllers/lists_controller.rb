2b62785e8993c606c351ac87d601fde276f30029
Patricia Kihara <patricia.kihara@outlook.com>
2021-12-19 23:28:13 -0300

    List index, show and create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,14 +3,29 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      @lists = List.all
+      render :index
+    end
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
