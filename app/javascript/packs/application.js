566c4ae727ac1743ddfd20e3f83e1dd23791e32b
Patricia Kihara <patricia.kihara@outlook.com>
2021-12-24 20:05:40 -0300

    Turbo setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 import 'bootstrap';
 import 'controllers';
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +19,6 @@ import 'controllers';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// document.addEventListener('turbo:load', () => {
+// })
